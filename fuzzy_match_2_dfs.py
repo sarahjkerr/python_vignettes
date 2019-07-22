a1855448bcb8cc9e2ee3ae91f8111e8b60c7cfce
Sarah K <sarah.j.kerr10@gmail.com>
2019-07-22 14:44:05 -0400

    Update fuzzy_match_2_dfs.py
    Replaced merging on 'Animal' and 'Match' column values to merging on testdata1_df index. 
    
    If there are more than 1 of the same entry in the original data set, this ensures the right instance of that animal will get merged.

--- fuzzy_match_2_dfs.py
+++ fuzzy_match_2_dfs.py
@@ -9,4 +9,4 @@ testdata2_df = pd.DataFrame.from_dict(testdata2)
 
 testdata2_df['matches'] = testdata2_df['Animal'].apply(lambda x: process.extractOne(x, testdata1_df['Animal'], scorer=fuzz.partial_ratio, score_cutoff=88))
 testdata2_df[['Match','Score','Data1Index']] = pd.DataFrame(testdata2_df['matches'].tolist(), index=testdata2_df.index)
-combo = testdata2_df.merge(testdata1_df, left_on='Match', right_on='Animal')
+combo = testdata2_df.merge(testdata1_df, left_on='Data1Index', right_index=True)
